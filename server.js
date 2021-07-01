96f7ec526f415b1a27e04aa7984d2af7c7ae8e6f
magdamk <75847988+magdamk@users.noreply.github.com>
2021-07-01 16:28:46 +0200

    once again for heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
 
-app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app2 = express();
+app2.use(serveStatic(path.join(__dirname, 'dist')));
 
-const port = process.env.PORT || 80
+const port = process.env.PORT || 80;
 
-app.listen(port)
+app2.listen(port);
